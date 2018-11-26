1f704c2ad6ace2b631fca12c4faef456c0da365a
bj123 <2389811519@qq.com>
2018-11-26 16:37:12 +0800

    登录

---
+++ step-core/src/main/java/com/aiwsport/core/service/storyService.java
@@ -0,0 +1,62 @@
+package com.aiwsport.core.service;
+
+import com.aiwsport.core.entity.User;
+import com.aiwsport.core.mapper.UserMapper;
+import com.aiwsport.core.utils.CommonUtil;
+import com.aiwsport.core.utils.DataTypeUtils;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yangjian9 on 2018/9/10.
+ */
+@Service
+public class StoryService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private static Logger logger = LogManager.getLogger();
+
+
+    public JSONObject decrypt(String encryptedData, String iv, String sessionKey){
+        return CommonUtil.decrypt(encryptedData, iv, sessionKey);
+    }
+
+    public User login(String openid, String province,
+                      String avatarUrl, String nickName, String country, String city, String gender) {
+        User user = userMapper.getByOpenId(openid);
+        if (user == null) {
+            // 创建用户
+            user = new User();
+            user.setOpenid(openid);
+            user.setCoinnum(0.00);
+            user.setAvatarurl(avatarUrl);
+            user.setCity(city);
+            user.setGender(gender);
+            user.setCountry(country);
+            user.setNickname(nickName);
+            user.setProvince(province);
+            user.setLikeid("");
+            user.setBuytemplateid("");
+            userMapper.insert(user);
+        } else {
+            if (!avatarUrl.equals(user.getAvatarurl())) {
+                user.setAvatarurl(avatarUrl);
+            }
+            if (!nickName.equals(user.getNickname())) {
+                user.setNickname(nickName);
+            }
+            userMapper.updateByPrimaryKey(user);
+        }
+        return user;
+    }
+
+}
