b6db4ad7a442a9c9d64096f93e25f24084629ff4
bj123 <2389811519@qq.com>
2018-12-03 13:24:58 +0800

    登录

---
+++ step-core/src/main/java/com/aiwsport/core/showmodel/ShareInfoObj.java
@@ -0,0 +1,31 @@
+package com.aiwsport.core.showmodel;
+
+import com.aiwsport.core.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by yangjian9 on 2018/12/3.
+ */
+public class ShareInfoObj {
+
+    private List<User> shareList;
+
+    private User myUser;
+
+    public List<User> getShareList() {
+        return shareList;
+    }
+
+    public void setShareList(List<User> shareList) {
+        this.shareList = shareList;
+    }
+
+    public User getMyUser() {
+        return myUser;
+    }
+
+    public void setMyUser(User myUser) {
+        this.myUser = myUser;
+    }
+}
